dbd3129b9ed48dfe3c23c34f7fb4092a6229e883
Pine <business@synthagen.net>
2024-10-10 13:17:22 -0400

    Update to fvtt 12

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 Hooks.on('ready', async () => {
-    _templateCache['systems/pf2e/templates/actors/character/limited.hbs'] = await getTemplate('systems/pf2e/templates/actors/character/sheet.hbs')
+    Handlebars.partials['systems/pf2e/templates/actors/character/limited.hbs'] = await getTemplate('systems/pf2e/templates/actors/character/sheet.hbs')
 })
